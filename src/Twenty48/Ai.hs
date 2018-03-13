b52a7979b5b19514b5fbe90e3f6110132796f343
Diogo Castro <dc@diogocastro.com>
2018-03-13 08:20:43 +0000

    Added alpha-beta pruning

--- src/Twenty48/Ai.hs
+++ src/Twenty48/Ai.hs
@@ -31,7 +31,7 @@ maximize :: StateTree Player Computer Score -> NonNull [Path]
 maximize StateTree{..} = 
   fromMaybe (singleton (Path [] root)) $ fromNullable maxs
     where
-      maxs = map maximum $ map minimize' forest
+      maxs = maxPrune $ map minimize' forest
 
       minimize' :: (Player, StateTree Computer Player Score) -> NonNull [Path]
       minimize' (player, sub) = mapNonNull (addTurn (P player)) $ minimize sub
@@ -40,10 +40,53 @@ minimize :: StateTree Computer Player Score -> NonNull [Path]
 minimize StateTree{..} =
   fromMaybe (singleton (Path [] root)) $ fromNullable mins
     where
-      mins = map minimum $ map maximize' forest      
+      mins = minPrune $ map maximize' forest      
 
       maximize' :: (Computer, StateTree Player Computer Score) -> NonNull [Path]
       maximize' (computer, sub) = mapNonNull (addTurn (C computer)) $ maximize sub
+
+-------------------------------------------------------
+-------------------------------------------------------
+
+maxPrune :: [NonNull [Path]] -> [Path]
+maxPrune [] = []
+maxPrune (xs : xss) =
+  min' : maxPrune' min' xss
+    where
+      min' = minimum xs
+
+maxPrune' :: Path -> [NonNull [Path]] -> [Path]
+maxPrune' p [] = []
+maxPrune' p (xs : xss)
+  | containsLeq p xs  = maxPrune' p xss
+  | otherwise         = min' : maxPrune' min' xss
+    where
+      min' = minimum xs
+
+containsLeq :: Path -> NonNull [Path] -> Bool
+containsLeq p = any (<= p)
+
+-------------------------------------------------------
+-------------------------------------------------------
+
+minPrune :: [NonNull [Path]] -> [Path]
+minPrune [] = []
+minPrune (xs : xss) =
+  max' : minPrune' max' xss
+    where
+      max' = maximum xs
+
+minPrune' :: Path -> [NonNull [Path]] -> [Path]
+minPrune' p []       = []
+minPrune' p (xs : xss)
+  | containsGeq p xs  = minPrune' p xss
+  | otherwise         = max' : minPrune' max' xss
+    where
+      max' = maximum xs
+      
+containsGeq :: Path -> NonNull [Path] -> Bool
+containsGeq p = any (>= p)
+
 
 printPaths :: NonNull [Path] -> IO ()
 printPaths = traverse_ printPath
