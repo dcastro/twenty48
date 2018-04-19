34a4f5c7b1589198b5126a3f647a516c8b976478
Diogo Castro <dc@diogocastro.com>
2018-04-19 12:39:11 +0100

    Added .ghcid file (ghcid v0.7)

--- benchmark/Bench.hs
+++ benchmark/Bench.hs
@@ -1,21 +1,20 @@
 {-# LANGUAGE OverloadedStrings #-}
 
-module Main where
+module Bench where
 
 import           ClassyPrelude
 import           Criterion.Main
+import qualified Data.Strict.Maybe    as M
 import           Game.AlphaBeta
 import           Game.Optimized.Board
 import           Game.Types
-import qualified Data.Strict.Maybe as M
 
-main :: IO ()
-main =
-  defaultMain
-    [ bgroup ("alpha-beta: find best move") $
-      flip map [5 .. 7] $ \depth ->
-        bench ("board4 depth: " <> show depth) $ nf (findBestMove depth) sampleBoard4
-    ]
+groups :: [Benchmark]
+groups =
+  [ bgroup ("alpha-beta: find best move") $
+    flip map [7 .. 7] $ \depth ->
+      bench ("board4 depth: " <> show depth) $ nf (findBestMove depth) sampleBoard4
+  ]
 
 findBestMove :: Int -> Board -> Int
 findBestMove depth b = M.fromJust . fmap (fromEnum . unPlayer) $ alphaBeta b depth
---
+++ benchmark/Main.hs
@@ -0,0 +1,8 @@
+module Main where
+
+import           Bench          (groups)
+import           ClassyPrelude
+import           Criterion.Main
+
+main :: IO ()
+main = defaultMain groups
