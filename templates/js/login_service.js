5be9488fe0e508e6009a87ee592e476a85c529a8
Diogo Castro <dc@diogocastro.com>
2026-02-25 22:25:35 +0000

    Migrate to GIS (Google Identity Services) / One Tap
    The legacy login method was not working on firefox:
    
    ```
    document.requestStorageAccess() may only be requested from inside a short running user-generated event handler
    ```
    
    This commit migrates to GIS.
    
    * https://developers.google.com/identity/gsi/web/guides/display-button#javascript
    * https://developers.google.com/identity/gsi/web/guides/display-google-one-tap#javascript
    * https://developers.google.com/identity/gsi/web/guides/features

--- templates/js/login_service.js
+++ templates/js/login_service.js
@@ -1,87 +1,95 @@
 function LoginService(onSignIn) {
+  const service = this;
+  let gisInitialized = false;
+  let currentProfile = null;
 
-  init();
+  // https://developers.google.com/identity/gsi/web/guides/display-google-one-tap#credential_response
+  function parseJwt(token) {
+    const base64Url = token.split('.')[1];
+    const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+    const jsonPayload = decodeURIComponent(atob(base64).split('').map(function (c) {
+      return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+    }).join(''));
+    return JSON.parse(jsonPayload);
+  }
+
+  function signedIn(profile) {
+    if (profile.picture) {
+      $("#avatar").attr('src', profile.picture);
+    }
+
+    $("#login-area .loading").hide();
+    $("#signed-out").hide();
+    $("#signed-in").show();
+
+    $("#save-score-button").hide();
+
+    $("body").addClass("signed-in");
+
+    onSignIn();
+  }
+
+  function signedOut() {
+    $("#login-area .loading").hide();
+    $("#signed-in").hide();
+    $("#signed-out").show();
+
+    $("body").removeClass("signed-in");
+  }
+
+  function ensureInitialized() {
+    if (gisInitialized) {
+      return;
+    }
+
+    google.accounts.id.initialize({
+      client_id: '587486773943-lb3cfne32q7t784ivehivj7rinjr4ds2.apps.googleusercontent.com',
+      callback: function (response) {
+        const payload = parseJwt(response.credential);
+        currentProfile = {
+          email: payload.email || null,
+          name: payload.name || null,
+          picture: payload.picture || null
+        };
+        signedIn(currentProfile);
+      }
+    });
+
+    gisInitialized = true;
+  }
 
   this.userDetails = function () {
-    return this.isSignedIn().then(function (signedIn) {
-
-      if (!signedIn)
-        return null;
-
-      const profile = gapi.auth2.getAuthInstance().currentUser.get().getBasicProfile();
-
-      return {
-        email: profile.getEmail(),
-        name: profile.getName()
-      };
-    });
+    return Promise.resolve(currentProfile);
   };
 
   this.isSignedIn = function () {
-    return whenLoaded().then(function () {
-      return gapi.auth2.getAuthInstance().isSignedIn.get();
-    });
+    return Promise.resolve(!!currentProfile);
   };
 
   this.signIn = function () {
-    return whenLoaded().then(function () {
-      return gapi.auth2.getAuthInstance().signIn();
-    });
-  }
+    service.button_clicked();
+  };
 
-  function whenLoaded() {
-    return new Promise(function (fulfilled, _) {
-      gapi.load('client:auth2', fulfilled);
-    });
-  }
+  this.signOut = function () {
+    currentProfile = null;
+    signedOut();
+  };
+
+  this.button_clicked = function () {
+    ensureInitialized();
+    google.accounts.id.prompt();
+  };
 
   function init() {
-    gapi.load('client:auth2', function () {
-      gapi.client.init({
-        'apiKey': 'AIzaSyDEmI3F231zE25CR_BYqRHsSqL16atW8KI',
-        'clientId': '587486773943-lb3cfne32q7t784ivehivj7rinjr4ds2.apps.googleusercontent.com',
-        'scope': 'profile'
-      }).then(function () {
-        const auth2 = gapi.auth2.getAuthInstance();
-
-        if (auth2.isSignedIn.get())
-          signedIn(auth2);
-        else
-          signedOut();
-
-        auth2.isSignedIn.listen(function (isSignedIn) {
-          if (isSignedIn)
-            signedIn(auth2);
-          else
-            signedOut();
-        });
-
-        $('#sign-in').click(auth2.signIn);
-        $('#sign-out').click(auth2.signOut);
-      });
+    $("#sign-in").click(function () {
+      service.button_clicked();
+    });
+    $("#sign-out").click(function () {
+      service.signOut();
     });
 
-    function signedIn(auth2) {
-      const profile = auth2.currentUser.get().getBasicProfile()
-      $("#avatar").attr('src', profile.getImageUrl());
-
-      $("#login-area .loading").hide();
-      $("#signed-out").hide();
-      $("#signed-in").show();
-
-      $("#save-score-button").hide();
-
-      $("body").addClass("signed-in");
-
-      onSignIn();
-    }
-
-    function signedOut() {
-      $("#login-area .loading").hide();
-      $("#signed-in").hide();
-      $("#signed-out").show();
-
-      $("body").removeClass("signed-in");
-    }
+    signedOut();
   }
+
+  init();
 }
